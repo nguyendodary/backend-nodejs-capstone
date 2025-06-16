7c1e2b8719fd319f5e026c272a1c3743e15f6db3
Nguyen Quoc Do <nguyenquocdodary@gmail.com>
2025-06-16 18:59:25 +0700

    Commit frontend changes before merging

--- secondchance-frontend/src/App.js
+++ secondchance-frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LandingPage from "./components/LandingPage";
+import MainPage from "./components/MainPage";
+import Register from "./components/Register";
+import ItemDetails from "./components/ItemDetails";
+import SearchResults from "./components/SearchResults";
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/items" element={<MainPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/items/:id" element={<ItemDetails />} />
+        <Route path="/search" element={<SearchResults />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
